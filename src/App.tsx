9491114e68b2fb968c7a08d78329b0f311deba88
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-29 15:37:05 +0100

    install and config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react'
-import './App.css'
 import getGithubAuthUrl from './utils/getGithubAuthUrl'
 import getGoogleAuthUrl from './utils/getGoogleAuthUrl'
 
@@ -9,15 +8,27 @@ function App() {
   }, [])
 
   return (
-    <div className="flex">
-      <a href={getGoogleAuthUrl()}>Log in by Google</a>
-      <a href={getGithubAuthUrl()}>Log in by GitHub</a>
-      <form className="flex">
-        <input type="email" autoComplete="email" />
-        <input type="password" autoComplete="current-password" />
-      </form>
+    <div className="dark:bg-black min-h-screen flex flex-col justify-center items-center gap-5">
+      <div className="dark:text-white space-x-5">
+        <a href={getGoogleAuthUrl()}>Log in by Google</a>
+        <a href={getGithubAuthUrl()}>Log in by GitHub</a>
+      </div>
 
-      <button>Log in</button>
+      <form className="flex flex-col gap-5 w-fit">
+        <input
+          type="email"
+          autoComplete="email"
+          className="dark:bg-gray-600 py-1 px-3"
+          placeholder="email"
+        />
+        <input
+          type="password"
+          autoComplete="current-password"
+          className="dark:bg-gray-600 py-1 px-3"
+          placeholder="password"
+        />
+        <button className="dark:bg-white py-1 px-3 rounded-full">Log in</button>
+      </form>
     </div>
   )
 }
