ed1f59fbd19882e6cd6de202d35cd943030e8c4b
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-29 15:29:05 +0100

    Add flex layout and login options

---
+++ src/utils/getGoogleAuthUrl.ts
@@ -0,0 +1,20 @@
+import env from './cleanEnv'
+
+const getGoogleAuthUrl = () => {
+  const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+  const options = {
+    redirect_uri: env.VITE_GOOGLE_REDIRECT_URL,
+    client_id: env.VITE_GOOGLE_CLIENT_ID,
+    access_type: 'online',
+    response_type: 'code',
+    prompt: 'consent',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+    ].join(' '),
+  }
+  const qs = new URLSearchParams(options)
+  return `${rootUrl}?${qs.toString()}`
+}
+
+export default getGoogleAuthUrl
