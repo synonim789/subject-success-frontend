400b6bc2b5b665d38f7e4962c7552ac61c41e914
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-29 15:28:54 +0100

    Add envalid package and cleanEnv utility

---
+++ src/utils/cleanEnv.ts
@@ -0,0 +1,9 @@
+import { cleanEnv, str } from 'envalid'
+
+export default cleanEnv(import.meta.env, {
+  VITE_GOOGLE_CLIENT_ID: str(),
+  VITE_GOOGLE_REDIRECT_URL: str(),
+  VITE_SERVER_ENDPOINT: str(),
+  VITE_GITHUB_CLIENT_ID: str(),
+  VITE_GITHUB_REDIRECT_URL: str(),
+})
