7f176ff4de029336cc900b8051ea8885e1d435fb
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-02 19:04:01 +0200

    Add Toaster component for displaying notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router.tsx'
@@ -8,6 +9,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster position="top-center" reverseOrder={true} />
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
