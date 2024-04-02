4321292beab114d321266be13e8de98425c38c1f
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-02 16:16:56 +0200

    Add custom text colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-black': '#2A323C',
+        'text-gray': '#737D8C',
+      },
+    },
   },
   plugins: [],
 }
