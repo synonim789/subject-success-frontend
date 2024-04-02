a03249d0527c8cf60edd19fc2f88cf23d589a0e0
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-02 16:17:50 +0200

    Refactor AuthLayout component to accept children prop

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { ReactNode } from 'react'
 
-const AuthLayout = () => {
+type AuthLayout = {
+  children: ReactNode
+}
+
+const AuthLayout = ({ children }: AuthLayout) => {
   return (
-    <section className="flex w-full">
-      <Outlet />
-      <div className="w-full">Backgroud will be here</div>
+    <section className="lg:flex">
+      <div className="bg-wh min-h-screen flex flex-col justify-center items-center gap-5 lg:w-1/3">
+        {children}
+      </div>
+
+      <div className="lg:w-2/3 auth-background hidden lg:block" />
     </section>
   )
 }
