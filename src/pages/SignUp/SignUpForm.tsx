9c800f4c0cc2e37bee3a04b8e013f01a6ad13595
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-04 17:35:44 +0200

    Add SignUpForm component and signUpSchema type definition

---
+++ src/pages/SignUp/SignUpForm.tsx
@@ -0,0 +1,87 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { MdAlternateEmail, MdDriveFileRenameOutline } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Input';
+import SubmitButton from '../../components/SubmitButton';
+import { SignUpFields, signUpSchema } from '../../types/signUpSchema';
+import { isFetchBaseQueryError } from '../../utils/isFetchBaseQueryError';
+
+const SignUpForm = () => {
+   const navigate = useNavigate();
+   //const [login, { isLoading, error }] = useLoginMutation()
+   const {
+      register,
+      handleSubmit,
+      formState: { errors, isSubmitting },
+      reset,
+   } = useForm<SignUpFields>({
+      resolver: zodResolver(signUpSchema),
+      mode: 'onBlur',
+   });
+
+   const submitHandler: SubmitHandler<SignUpFields> = async (data) => {
+      try {
+         // const accessToken = await login({
+         //   email: data.email,
+         //   password: data.password,
+         // }).unwrap()
+         navigate('/login');
+         toast.success('Now you have to log in!');
+         reset();
+      } catch (err) {
+         if (isFetchBaseQueryError(err)) {
+            const errMsg = (err as { data: { message: string } }).data.message;
+            toast.error(errMsg);
+         }
+         console.log(err);
+      }
+   };
+
+   return (
+      <>
+         <form onSubmit={handleSubmit(submitHandler)} className="w-full px-5">
+            <Input
+               type="text"
+               placeholder="Enter your email..."
+               label="Email adress"
+               name="email"
+               id="email"
+               icon={<MdAlternateEmail size={20} />}
+               register={{ ...register('email') }}
+               error={errors.email}
+            />
+            <Input
+               type="text"
+               placeholder="Enter your username..."
+               label="username"
+               name="username"
+               id="username"
+               icon={<MdDriveFileRenameOutline size={20} />}
+               register={{ ...register('username') }}
+               error={errors.username}
+            />
+            <Input
+               type="password"
+               placeholder="**********"
+               label="Password"
+               name="password"
+               id="password"
+               isPassword={true}
+               register={{ ...register('password') }}
+               error={errors.password}
+            />
+            <SubmitButton text="login" disabled={isSubmitting} />
+            {/* {error && (
+          <p className="text-left text-red-500 font-semibold mt-2">
+            {'status' in error
+              ? (error as { data: { message: string } }).data.message
+              : error.message}
+          </p>
+        )} */}
+         </form>
+      </>
+   );
+};
+export default SignUpForm;
