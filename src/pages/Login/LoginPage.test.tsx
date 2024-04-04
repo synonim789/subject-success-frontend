2209351ae9c773ecb45f5ce6533c4b9f75dcc9af
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-04 17:07:09 +0200

    Refactor login tests and add new test cases

--- src/pages/Login/LoginPage.test.tsx
+++ src/pages/Login/LoginPage.test.tsx
@@ -1,49 +1,97 @@
 import userEvent from '@testing-library/user-event'
-import { render, screen, waitFor } from '../../test/test-utils'
+import { mockServer } from '../../test/server/server'
+import { render, screen } from '../../test/test-utils'
 import LoginPage from './LoginPage'
 
-const mockedUseNavigate = vi.fn()
-vi.mock('react-router-dom', async () => {
-  const mod = await vi.importActual<typeof import('react-router-dom')>(
-    'react-router-dom'
-  )
-  return {
-    ...mod,
-    useNavigate: () => mockedUseNavigate,
-  }
-})
+mockServer()
 
 describe('Login Test', () => {
-  it('should redirect to home page when user is logged in correctly', async () => {
-    const user = userEvent.setup()
-    render(<LoginPage />)
-    const emailInput = screen.getByLabelText(/email/i)
-    expect(emailInput).toBeInTheDocument()
+  describe('with valid inputs', () => {
+    it('should redirect to home page', async () => {
+      const user = userEvent.setup()
+      render(<LoginPage />)
+      const emailInput = screen.getByLabelText(/email/i)
+      expect(emailInput).toBeInTheDocument()
 
-    const passwordInput = screen.getByLabelText(/password/i)
-    expect(passwordInput).toBeInTheDocument()
+      const passwordInput = screen.getByLabelText(/password/i)
+      expect(passwordInput).toBeInTheDocument()
 
-    const loginButton = screen.getByRole('button', { name: /login/i })
-    expect(loginButton).toBeInTheDocument()
+      const loginButton = screen.getByRole('button', { name: /login/i })
+      expect(loginButton).toBeInTheDocument()
 
-    await user.type(emailInput, 'test@gmail.com')
-    await user.type(passwordInput, 'Test12345')
+      await user.type(emailInput, 'test@gmail.com')
+      await user.type(passwordInput, 'Test12345')
 
-    await user.click(loginButton)
+      await user.click(loginButton)
 
-    waitFor(() => {
-      expect(mockedUseNavigate).toBeCalledWith('/')
+      expect(window.location.pathname).toBe('/')
     })
   })
-  it('should show invalid email if email is not valid', async () => {
-    const user = userEvent.setup()
-    render(<LoginPage />)
-    const emailInput = screen.getByLabelText(/email/i)
-    await user.type(emailInput, 'test.com')
+  describe('with invalid email', () => {
+    it('should show invalid email if email is not valid', async () => {
+      const user = userEvent.setup()
+      render(<LoginPage />)
+      const emailInput = screen.getByLabelText(/email/i)
+      await user.type(emailInput, 'test.com')
 
-    const loginButton = screen.getByRole('button', { name: /login/i })
-    await user.click(loginButton)
+      const passwordInput = screen.getByLabelText(/password/i)
+      await user.click(passwordInput)
 
-    expect(await screen.findAllByRole('alert')).toHaveLength(2)
+      expect(await screen.findByText(/invalid email/i)).toBeInTheDocument()
+    })
+  })
+
+  describe('with invalid password', () => {
+    it('should render password is to short validation error', async () => {
+      const user = userEvent.setup()
+      render(<LoginPage />)
+
+      const passwordInput = screen.getByLabelText(/password/i)
+      const emailInput = screen.getByLabelText(/email/i)
+      await user.type(passwordInput, 'test123')
+      await user.click(emailInput)
+
+      expect(
+        await screen.findByText(/password should be at least 8 characters/i)
+      ).toBeInTheDocument()
+    })
+    it('should render password do not match criteria', async () => {
+      const user = userEvent.setup()
+      render(<LoginPage />)
+
+      const passwordInput = screen.getByLabelText(/password/i)
+      const emailInput = screen.getByLabelText(/email/i)
+      await user.type(passwordInput, 'test12345')
+      await user.click(emailInput)
+
+      expect(
+        await screen.findByText(
+          /use at least 8 characters one uppercase letter one lowercase letter and one number in your password/i
+        )
+      ).toBeInTheDocument()
+    })
+  })
+
+  describe('with valid inputs but not existing on server', () => {
+    it('should return error message', async () => {
+      const user = userEvent.setup()
+
+      render(<LoginPage />)
+
+      const emailInput = screen.getByLabelText(/email/i)
+      const passwordInput = screen.getByLabelText(/password/i)
+      const loginButton = screen.getByRole('button', { name: /login/i })
+
+      // Fill in the form fields
+      await user.type(emailInput, 'test2@gmail.com')
+      await user.type(passwordInput, 'Test12345')
+      // Submit the form
+      await user.click(loginButton)
+
+      // Wait for the error message to appear
+      expect(
+        await screen.findByText(/invalid credentials/i)
+      ).toBeInTheDocument()
+    })
   })
 })
