69f0dffd8ea31458cc814fc6fe2227ca68c63415
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-04 17:06:29 +0200

    Add type attribute to submit button

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -8,6 +8,7 @@ const SubmitButton = ({ text, disabled }: SubmitButton) => {
     <button
       className="bg-green-500 uppercase w-full py-2 rounded text-white hover:bg-green-400 transition-all duration-300"
       disabled={disabled}
+      type="submit"
     >
       {text}
     </button>
