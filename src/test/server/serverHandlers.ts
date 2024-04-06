f70d833994e8605d062ace43ebe6c776283422d9
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-06 17:31:18 +0200

    Add sign-up functionality to serverHandlers.ts

--- src/test/server/serverHandlers.ts
+++ src/test/server/serverHandlers.ts
@@ -5,13 +5,19 @@ type LoginRequestBody = {
    password: string;
 };
 
+type SignUpRequestBody = {
+   email: string;
+   username: string;
+   password: string;
+};
+
 export const handlers = [
    http.post<
       PathParams,
       LoginRequestBody,
       string | { message: string },
       'http://localhost:3000/auth/login'
-   >('http://localhost:3000/auth/login', async ({ params, request }) => {
+   >('http://localhost:3000/auth/login', async ({ request }) => {
       const { email } = await request.json();
       if (email === 'test2@gmail.com') {
          return HttpResponse.json(
@@ -20,5 +26,20 @@ export const handlers = [
          );
       }
       return HttpResponse.json('yctycytcy', { status: 200 });
+   }),
+   http.post<
+      PathParams,
+      SignUpRequestBody,
+      { message: string },
+      'http://localhost:3000/user/sign-up'
+   >('http://localhost:3000/user/sign-up', async ({ request }) => {
+      const { email } = await request.json();
+      if (email === 'test2@gmail.com') {
+         return HttpResponse.json(
+            { message: 'User already exist' },
+            { status: 401 },
+         );
+      }
+      return HttpResponse.json({ message: 'Sign up success' }, { status: 200 });
    }),
 ];
