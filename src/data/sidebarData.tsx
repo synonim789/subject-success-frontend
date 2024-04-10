58387101f11a703ffc209b46695f4fc78afc4ed3
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-10 16:55:22 +0200

    Add sidebar data

---
+++ src/data/sidebarData.tsx
@@ -0,0 +1,12 @@
+import { FaSchool, FaTasks } from 'react-icons/fa';
+
+export const sidebarData = [
+   {
+      name: 'subjects',
+      icon: <FaSchool />,
+   },
+   {
+      name: 'tasks',
+      icon: <FaTasks />,
+   },
+];
