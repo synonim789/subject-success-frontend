bc6b6696ed91f0e208be1d27ad889ab0e377240d
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-10 16:55:06 +0200

    Refactor Dash component to include a fixed sidebar and navbar

--- src/pages/Dash/Dash.tsx
+++ src/pages/Dash/Dash.tsx
@@ -1,6 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import DashNavbar from '../../components/DashNavbar';
+import DashSidebar from '../../components/DashSidebar';
 
 const Dash = () => {
-   return <Link to="/login">Link to Login</Link>;
+   return (
+      <div className="relative flex">
+         <div className="fixed w-80 dark:bg-dark-800">
+            <DashSidebar className="relative ml-3 h-screen overflow-auto pb-10 md:overflow-hidden md:hover:overflow-auto" />
+         </div>
+         <div className="min-h-screen w-full md:ml-80">
+            <div className="fixed w-full bg-dark-900 md:static">
+               <DashNavbar className="relative flex justify-between p-2 text-white/85 md:mx-6" />
+               <div>
+                  <Outlet />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 };
 export default Dash;
