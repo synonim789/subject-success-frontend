372a84c2002fdf5284cc1011c183ad48ca32dea7
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-11 14:16:39 +0200

    Add motion and useState imports, and implement sidebar toggle functionality

--- src/layout/DashLayout.tsx
+++ src/layout/DashLayout.tsx
@@ -1,16 +1,36 @@
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import DashNavbar from '../components/DashNavbar';
 import DashSidebar from '../components/DashSidebar';
 
 const DashLayout = () => {
+   const [sidebarVisible, setSidebarVisible] = useState(true);
+
    return (
       <div className="relative flex">
-         <div className="fixed w-80 dark:bg-dark-800">
-            <DashSidebar className="relative ml-3 h-screen overflow-auto pb-10 md:overflow-hidden md:hover:overflow-auto" />
-         </div>
-         <div className="min-h-screen w-full md:ml-80">
-            <div className="fixed w-full bg-dark-900 md:static">
-               <DashNavbar className="relative flex justify-between p-2 text-white/85 md:mx-6" />
+         <motion.div
+            initial={false}
+            animate={{
+               width: sidebarVisible ? 320 : 0,
+            }}
+            className={`fixed z-[1000] bg-white dark:bg-dark-800`}
+         >
+            <DashSidebar
+               className="relative  h-screen overflow-auto pb-10 pl-3 shadow-2xl md:overflow-hidden md:hover:overflow-auto"
+               sidebarVisible={sidebarVisible}
+               setSidebarVisible={setSidebarVisible}
+            />
+         </motion.div>
+         <div
+            className={`${sidebarVisible ? 'md:ml-80' : 'flex-2'} min-h-screen w-full`}
+         >
+            <div className="fixed w-full  md:static dark:bg-dark-900">
+               <DashNavbar
+                  className="relative flex items-center justify-between p-2 text-slate-800 md:mx-6 dark:text-white/85"
+                  sidebarVisible={sidebarVisible}
+                  setSidebarVisible={setSidebarVisible}
+               />
                <div>
                   <Outlet />
                </div>
