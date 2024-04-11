cc0e753205299620e27a0407bfef355518d70273
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-11 17:00:20 +0200

    Add framer-motion animation to NavbarDropdown

--- src/components/NavbarDropdown.tsx
+++ src/components/NavbarDropdown.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { CiLogout } from 'react-icons/ci';
 import { LuMoon, LuSun } from 'react-icons/lu';
@@ -36,7 +37,7 @@ const NavbarDropdown = () => {
    });
 
    return (
-      <div className="relative">
+      <div className="relative" ref={dropdownMenu}>
          <button
             className="group flex items-center justify-center gap-2 rounded-lg p-2 hover:bg-green-600 "
             type="button"
@@ -46,54 +47,57 @@ const NavbarDropdown = () => {
             <p className="text-lg transition group-hover:text-white">Oskar</p>
             <MdKeyboardArrowDown className=" text-gray-400 transition group-hover:text-white" />
          </button>
-         {showDropdown && (
-            <div
-               className="mt-2divide-y absolute right-0 z-10 w-52  divide-gray-100 rounded-md bg-white shadow-lg  dark:bg-dark-500"
-               ref={dropdownMenu}
-            >
-               <div className="py-2">
-                  <Link
-                     to="/profile"
-                     className="block px-4 py-2 text-base transition hover:bg-gray-100 dark:hover:bg-dark-200"
-                     onClick={() => setShowDropdown(false)}
-                  >
-                     Profile
-                  </Link>
-                  <div className="flex items-center gap-2 px-4 py-2 transition hover:bg-gray-100 dark:hover:bg-dark-200">
-                     <label
-                        htmlFor="check"
-                        className="relative block h-8 w-16 cursor-pointer rounded-full bg-gray-100 dark:bg-dark-900"
+         <AnimatePresence>
+            {showDropdown && (
+               <motion.div
+                  className="absolute right-0 z-10 mt-2 w-52 divide-y  divide-gray-100 rounded-md bg-white shadow-lg  dark:bg-dark-500"
+                  initial={{ opacity: 0, y: 0 }}
+                  animate={{ opacity: 1, y: 1 }}
+               >
+                  <div className="py-2">
+                     <Link
+                        to="/profile"
+                        className="block px-4 py-2 text-base transition hover:bg-gray-100 dark:hover:bg-dark-200"
+                        onClick={() => setShowDropdown(false)}
                      >
-                        <input
-                           type="checkbox"
-                           className="peer sr-only"
-                           name="check"
-                           id="check"
-                           checked={isDarkMode}
-                           onChange={(e) => setIsDarkMode(e.target.checked)}
-                        />
-                        <span className="absolute left-1 top-1 flex items-center justify-center rounded-full bg-black  transition-all duration-500 peer-checked:left-9 peer-checked:bg-[#F4F3F2]">
-                           {isDarkMode ? (
-                              <LuMoon className="p-1 text-dark-900" />
-                           ) : (
-                              <LuSun className="p-1 text-white" />
-                           )}
-                        </span>
-                     </label>
-                     <p className="text-base">Dark mode</p>
-                  </div>
+                        Profile
+                     </Link>
+                     <div className="flex items-center gap-2 px-4 py-2 transition hover:bg-gray-100 dark:hover:bg-dark-200">
+                        <label
+                           htmlFor="check"
+                           className="relative block h-8 w-16 cursor-pointer rounded-full bg-gray-100 dark:bg-dark-900"
+                        >
+                           <input
+                              type="checkbox"
+                              className="peer sr-only"
+                              name="check"
+                              id="check"
+                              checked={isDarkMode}
+                              onChange={(e) => setIsDarkMode(e.target.checked)}
+                           />
+                           <span className="absolute left-1 top-1 flex items-center justify-center rounded-full bg-black  transition-all duration-500 peer-checked:left-9 peer-checked:bg-[#F4F3F2]">
+                              {isDarkMode ? (
+                                 <LuMoon className="p-1 text-dark-900" />
+                              ) : (
+                                 <LuSun className="p-1 text-white" />
+                              )}
+                           </span>
+                        </label>
+                        <p className="text-base">Dark mode</p>
+                     </div>
 
-                  <button
-                     className="flex w-full items-center gap-2 px-4 py-2 text-left text-base text-red-400 transition-none hover:bg-gray-100 dark:hover:bg-dark-200"
-                     type="button"
-                     onClick={handleLogout}
-                  >
-                     <CiLogout />
-                     <span>Logout</span>
-                  </button>
-               </div>
-            </div>
-         )}
+                     <button
+                        className="flex w-full items-center gap-2 px-4 py-2 text-left text-base text-red-400 transition-none hover:bg-gray-100 dark:hover:bg-dark-200"
+                        type="button"
+                        onClick={handleLogout}
+                     >
+                        <CiLogout />
+                        <span>Logout</span>
+                     </button>
+                  </div>
+               </motion.div>
+            )}
+         </AnimatePresence>
       </div>
    );
 };
