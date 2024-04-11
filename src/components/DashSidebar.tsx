2d161ec9b80e1305992f60dd439ba080b1cdc08a
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-11 17:00:14 +0200

    Refactor DashSidebar component

--- src/components/DashSidebar.tsx
+++ src/components/DashSidebar.tsx
@@ -33,55 +33,56 @@ const DashSidebar = ({
 
    return (
       <nav className={`${className} h-full`}>
-         <AnimatePresence></AnimatePresence>
-         {sidebarVisible && (
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-               <div className="flex items-center justify-between">
-                  <Link
-                     to="/"
-                     className="group ml-3 mt-4 flex items-center gap-3 text-xl font-extrabold text-slate-500   dark:text-white/85"
-                  >
-                     <span className="rounded-full border-2  border-slate-500 transition-colors group-hover:border-green-house-400 group-hover:text-green-house-400 dark:border-white/85">
-                        <BsCheck />
-                     </span>{' '}
-                     <span className="transition-colors group-hover:text-green-house-400">
-                        Subject Success
-                     </span>
-                  </Link>
-                  <button
-                     className="mt-4 block rounded-full p-3 text-xl text-slate-800 hover:bg-gray-300 md:hidden dark:text-white/85"
-                     type="button"
-                     onClick={() => setSidebarVisible(!sidebarVisible)}
-                  >
-                     <MdOutlineCancel />
-                  </button>
-               </div>
-               <div className="mt-10 flex  flex-col justify-between">
-                  <div>
-                     {sidebarData.map((link) => (
-                        <NavLink
-                           key={link.name}
-                           to={`/${link.name}`}
-                           className="m-2 flex items-center gap-5 rounded-lg pb-2.5 pl-4 pt-3 text-lg text-gray-700 transition hover:bg-green-house-600 hover:text-white dark:text-white/60"
-                        >
-                           {link.icon}
-                           <span className="capitalize">{link.name}</span>
-                        </NavLink>
-                     ))}
+         <AnimatePresence>
+            {sidebarVisible && (
+               <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                  <div className="flex items-center justify-between">
+                     <Link
+                        to="/"
+                        className="group ml-3 mt-4 flex items-center gap-3 text-xl font-extrabold text-slate-500   dark:text-white/85"
+                     >
+                        <span className="rounded-full border-2  border-slate-500 transition-colors group-hover:border-green-house-400 group-hover:text-green-house-400 dark:border-white/85">
+                           <BsCheck />
+                        </span>{' '}
+                        <span className="transition-colors group-hover:text-green-house-400">
+                           Subject Success
+                        </span>
+                     </Link>
+                     <button
+                        className="mr-2 mt-4 block rounded-full p-3 text-2xl text-slate-800 hover:bg-gray-300 md:hidden dark:text-white/85"
+                        type="button"
+                        onClick={() => setSidebarVisible(!sidebarVisible)}
+                     >
+                        <MdOutlineCancel />
+                     </button>
                   </div>
-               </div>
-               <div className="absolute bottom-5 left-0 right-0 mx-auto">
-                  <button
-                     className="flex w-full items-center justify-center gap-2 text-xl font-semibold text-red-500 transition hover:text-red-600 dark:text-red-400 dark:hover:text-red-500"
-                     type="button"
-                     onClick={handleLogout}
-                  >
-                     <CiLogout />
-                     <span>Logout</span>
-                  </button>
-               </div>
-            </motion.div>
-         )}
+                  <div className="mt-10 flex  flex-col justify-between">
+                     <div>
+                        {sidebarData.map((link) => (
+                           <NavLink
+                              key={link.name}
+                              to={`/${link.name}`}
+                              className="m-2 flex items-center gap-5 rounded-lg pb-2.5 pl-4 pt-3 text-lg text-gray-700 transition hover:bg-green-house-600 hover:text-white dark:text-white/60"
+                           >
+                              {link.icon}
+                              <span className="capitalize">{link.name}</span>
+                           </NavLink>
+                        ))}
+                     </div>
+                  </div>
+                  <div className="absolute bottom-5 left-0 right-0 mx-auto">
+                     <button
+                        className="flex w-full items-center justify-center gap-2 text-xl font-semibold text-red-500 transition hover:text-red-600 dark:text-red-400 dark:hover:text-red-500"
+                        type="button"
+                        onClick={handleLogout}
+                     >
+                        <CiLogout />
+                        <span>Logout</span>
+                     </button>
+                  </div>
+               </motion.div>
+            )}
+         </AnimatePresence>
       </nav>
    );
 };
