91e36d104ac023a1e7ab3fb1df7c74d55c1e1e17
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-16 15:18:00 +0200

    Refactor ProfilePage component

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,8 +1,8 @@
 import { CiEdit } from 'react-icons/ci';
 import { useGetUserQuery } from '../../app/api/userApiSlice';
 import Header from '../../components/Header';
-import ProfileInput from '../../components/ProfileInput';
-import SubmitButton from '../../components/SubmitButton';
+import SetNewPasswordForm from './SetNewPasswordForm';
+import UpdateUserInfo from './UpdateUserInfo';
 import ProfileImagePlaceholder from '/profile-placeholder.jpg';
 
 const ProfilePage = () => {
@@ -49,47 +49,8 @@ const ProfilePage = () => {
             </div>
          </section>
          <section className="flex w-full flex-col gap-10 lg:flex-row">
-            <section className=" w-full rounded-xl bg-white/80 p-10 drop-shadow-xl  dark:bg-dark-600">
-               <ProfileInput
-                  name="username"
-                  type="text"
-                  label="Username"
-                  id="username"
-                  placeholder="Enter Username"
-                  register=""
-               />
-               <ProfileInput
-                  name="Email"
-                  type="email"
-                  label="Email"
-                  id="email"
-                  placeholder="Enter Email"
-                  register=""
-                  disabled
-               />
-               <SubmitButton text="update user" disabled={false} />
-            </section>
-            <section className=" grid w-full rounded-xl bg-white/80 p-10 drop-shadow-xl  dark:bg-dark-600">
-               <ProfileInput
-                  name="Password"
-                  type="password"
-                  label="Password"
-                  id="password"
-                  placeholder="Enter Password"
-                  register=""
-                  isPassword
-               />
-               <ProfileInput
-                  name="confirmPassword"
-                  type="password"
-                  label="Confirm Password"
-                  id="confirmPassword"
-                  placeholder="Re-enter Password"
-                  register=""
-                  isPassword
-               />
-               <SubmitButton text="Reset Password" disabled={false} />
-            </section>
+            <UpdateUserInfo />
+            <SetNewPasswordForm />
          </section>
       </section>
    );
