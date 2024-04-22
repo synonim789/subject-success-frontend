69a0b3f266254f220e41ed174d9e8d31a4380e54
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-04-22 20:23:43 +0200

    Add DashLayout test file

---
+++ src/layout/DashLayout.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '../test/test-utils';
+import DashLayout from './DashLayout';
+
+describe('Dash Layout Test', () => {
+   it('should render', () => {
+      render(<DashLayout />);
+   });
+});
