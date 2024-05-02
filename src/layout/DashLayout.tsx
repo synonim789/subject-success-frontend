19a7f95f1e864ea20846c9979985e309ce677e35
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-02 15:28:12 +0200

    Fix sidebar overflow issue

--- src/layout/DashLayout.tsx
+++ src/layout/DashLayout.tsx
@@ -17,7 +17,7 @@ const DashLayout = () => {
             className={`fixed z-[1000] bg-white dark:bg-dark-800`}
          >
             <DashSidebar
-               className="relative h-screen overflow-auto pb-10 pl-3 shadow-2xl md:overflow-hidden md:hover:overflow-auto"
+               className="relative h-screen overflow-auto pb-10 pl-3 shadow-2xl md:hover:overflow-auto"
                sidebarVisible={sidebarVisible}
                setSidebarVisible={setSidebarVisible}
             />
@@ -25,7 +25,7 @@ const DashLayout = () => {
          <div
             className={`${sidebarVisible ? 'md:ml-80' : 'flex-2'} min-h-screen w-full`}
          >
-            <div className="fixed w-full  md:static dark:bg-dark-900">
+            <div className="static  w-full dark:bg-dark-900">
                <DashNavbar
                   className="relative flex items-center justify-between p-2 text-slate-800 md:mx-6 dark:text-white/85"
                   sidebarVisible={sidebarVisible}
