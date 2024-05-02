16f9b408ca8643446ec3c036a6c3a9ef8f8de00e
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-02 16:24:52 +0200

    Remove header component from SubjectsPage

--- src/pages/Subjects/SubjectsPage.tsx
+++ src/pages/Subjects/SubjectsPage.tsx
@@ -1,10 +1,8 @@
-import Header from '../../components/Header';
 import Subjects from './Subjects';
 
 const SubjectsPage = () => {
    return (
       <section>
-         <Header text="subjects" />
          <div className="flex w-full flex-col flex-wrap gap-10 md:flex-row">
             <Subjects />
          </div>
