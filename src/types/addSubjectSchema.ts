109762cc0eee3ff18806bfc25cc3f0b221347cc9
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-08 12:43:41 +0200

    Update addSubjectSchema to include new subject types

--- src/types/addSubjectSchema.ts
+++ src/types/addSubjectSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const addSubjectSchema = z.object({
    name: z.string(),
-   type: z.string(),
+   type: z.enum(['grade', 'completion']),
 });
 
 export type AddSubjectFields = z.infer<typeof addSubjectSchema>;
