cb29e0365be09635fe9daba9426bb709843a50aa
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-08 13:32:15 +0200

    Refactor import in AddSubject.tsx

--- src/pages/Subjects/AddSubject.tsx
+++ src/pages/Subjects/AddSubject.tsx
@@ -1,11 +1,11 @@
-import AddSubjectModalContent from '../../components/AddSubjectModalContent';
 import ModalComponent from '../../components/Modal';
+import AddSubjectModalContent from './AddSubjectModalContent';
 
 const AddSubject = () => {
    return (
       <>
          <ModalComponent
-            buttonClassName="flex h-[500px] min-w-[300px] cursor-pointer items-center justify-center rounded-xl border-2 border-dashed border-gray-600 text-xl font-bold text-gray-600 transition hover:border-gray-300"
+            buttonClassName="flex min-h-[500px] min-w-[300px] cursor-pointer items-center justify-center rounded-xl border-2 border-dashed border-gray-600 text-xl font-bold text-gray-600 transition hover:border-gray-300"
             buttonChildren="Add Subject +"
             children={<AddSubjectModalContent />}
          />
