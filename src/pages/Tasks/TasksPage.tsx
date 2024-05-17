f8a89821609e309a910c47c8717a91b62bbfa9ef
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-17 14:06:04 +0200

    Add task list to TasksPage

--- src/pages/Tasks/TasksPage.tsx
+++ src/pages/Tasks/TasksPage.tsx
@@ -1,9 +1,22 @@
+import { useGetAllTaskQuery } from '../../app/api/taskApiSlice';
 import Header from '../../components/Header';
+import SubjectTask from '../Subjects/SubjectTask';
 
 const TasksPage = () => {
+   const { data: tasks } = useGetAllTaskQuery();
+   if (!tasks) {
+      return <p>No Task found</p>;
+   }
+
+   console.log(tasks);
    return (
       <section>
          <Header text="tasks" />
+         <section className="grid gap-5  sm:grid-cols-4">
+            {tasks.map((task) => {
+               return <SubjectTask task={task} key={task._id} />;
+            })}
+         </section>
       </section>
    );
 };
