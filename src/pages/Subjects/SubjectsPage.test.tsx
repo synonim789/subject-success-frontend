05ad8f38bc659138e7b65dc73d574ef45c7fc3e4
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-05-24 20:47:01 +0200

    add Tests for subject Page and add server handlers for it

---
+++ src/pages/Subjects/SubjectsPage.test.tsx
@@ -0,0 +1,233 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen, within } from '../../test/test-utils';
+import SubjectsPage from './SubjectsPage';
+
+describe('Subjects Page Test', () => {
+   it('should be rendered', async () => {
+      render(<SubjectsPage />);
+      const subjects = await screen.findAllByLabelText('subject');
+      expect(subjects).toHaveLength(3);
+   });
+   it('should add subject when addsubject form is submitted', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const addSubjectBtn = await screen.findByRole('button', {
+         name: /add subject/i,
+      });
+      expect(addSubjectBtn).toBeInTheDocument();
+
+      await user.click(addSubjectBtn);
+
+      const nameInput = screen.getByLabelText(/name/i);
+      expect(nameInput).toBeInTheDocument();
+
+      await user.type(nameInput, 'test1234');
+
+      const select = screen.getByLabelText(/choose type/i);
+      expect(select).toBeInTheDocument();
+
+      await user.click(select);
+      expect(select).toHaveAttribute('data-pressed', 'true');
+
+      const listbox = screen.getByRole('listbox');
+      expect(listbox).toBeInTheDocument();
+
+      const options = within(listbox).getAllByRole('option');
+
+      expect(options).toHaveLength(2);
+
+      await user.click(options[1]);
+      expect(select).toHaveTextContent('Completion');
+
+      const submitButton = await screen.findByRole('button', {
+         name: /add subject/i,
+      });
+
+      expect(submitButton).toBeInTheDocument();
+
+      user.click(submitButton);
+
+      expect(
+         await screen.findByText('Subject added successfully'),
+      ).toBeInTheDocument();
+   });
+   it('should return toast with error if there is one', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const addSubjectBtn = await screen.findByRole('button', {
+         name: /add subject/i,
+      });
+
+      await user.click(addSubjectBtn);
+
+      const nameInput = screen.getByLabelText(/name/i);
+
+      await user.type(nameInput, 'test123');
+
+      const select = screen.getByLabelText(/choose type/i);
+
+      await user.click(select);
+
+      const listbox = screen.getByRole('listbox');
+
+      const options = within(listbox).getAllByRole('option');
+
+      await user.click(options[1]);
+
+      const submitButton = await screen.findByRole('button', {
+         name: /add subject/i,
+      });
+
+      user.click(submitButton);
+      expect(await screen.findByText('there was an error')).toBeInTheDocument();
+   });
+   it('subject should be edited with grade type', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const subjects = await screen.findAllByLabelText('subject');
+      expect(subjects).toHaveLength(3);
+      const subject = subjects[0];
+
+      const optionsBtn = await within(subject).findByLabelText(/open options/i);
+      await user.click(optionsBtn);
+
+      const editBtn = await screen.findByLabelText(/edit modal/i);
+      expect(editBtn).toBeInTheDocument();
+      await userEvent.click(editBtn);
+
+      const nameInput = await screen.findByLabelText(/name/i);
+      expect(nameInput).toBeInTheDocument();
+
+      await user.type(nameInput, 'test1234');
+
+      const select = screen.getByLabelText(/choose type/i);
+      expect(select).toBeInTheDocument();
+
+      await user.click(select);
+      expect(select).toHaveAttribute('data-pressed', 'true');
+
+      const listbox = screen.getByRole('listbox');
+      expect(listbox).toBeInTheDocument();
+
+      const options = within(listbox).getAllByRole('option');
+
+      expect(options).toHaveLength(2);
+
+      await user.click(options[0]);
+      expect(select).toHaveTextContent('Grade');
+
+      const labelInuput = await screen.findByLabelText(/grade number/i);
+      await user.type(labelInuput, '1.0');
+
+      const submitBtn = await screen.findByRole('button', {
+         name: /edit subject/i,
+      });
+      await user.click(submitBtn);
+   });
+   it('should return toast with error if there is one while editing subject', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const subjects = await screen.findAllByLabelText('subject');
+      expect(subjects).toHaveLength(3);
+      const subject = subjects[0];
+
+      const optionsBtn = await within(subject).findByLabelText(/open options/i);
+      await user.click(optionsBtn);
+
+      const editBtn = await screen.findByLabelText(/edit modal/i);
+      expect(editBtn).toBeInTheDocument();
+      await userEvent.click(editBtn);
+
+      const nameInput = await screen.findByLabelText(/name/i);
+      expect(nameInput).toBeInTheDocument();
+
+      await user.type(nameInput, 'test123');
+
+      const select = screen.getByLabelText(/choose type/i);
+      expect(select).toBeInTheDocument();
+
+      await user.click(select);
+      expect(select).toHaveAttribute('data-pressed', 'true');
+
+      const listbox = screen.getByRole('listbox');
+      expect(listbox).toBeInTheDocument();
+
+      const options = within(listbox).getAllByRole('option');
+
+      expect(options).toHaveLength(2);
+
+      await user.click(options[0]);
+      expect(select).toHaveTextContent('Grade');
+
+      const labelInuput = await screen.findByLabelText(/grade number/i);
+      await user.type(labelInuput, '1.0');
+
+      const submitBtn = await screen.findByRole('button', {
+         name: /edit subject/i,
+      });
+      await user.click(submitBtn);
+
+      expect(await screen.findByText('there was an error')).toBeInTheDocument();
+   });
+   it('subject should be edited with completion type', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const subjects = await screen.findAllByLabelText('subject');
+      expect(subjects).toHaveLength(3);
+      const subject = subjects[0];
+
+      const optionsBtn = await within(subject).findByLabelText(/open options/i);
+      await user.click(optionsBtn);
+
+      const editBtn = await screen.findByLabelText(/edit modal/i);
+      expect(editBtn).toBeInTheDocument();
+      await userEvent.click(editBtn);
+
+      const nameInput = await screen.findByLabelText(/name/i);
+      expect(nameInput).toBeInTheDocument();
+
+      await user.type(nameInput, 'test1234');
+
+      const select = screen.getByLabelText(/choose type/i);
+      expect(select).toBeInTheDocument();
+
+      await user.click(select);
+      expect(select).toHaveAttribute('data-pressed', 'true');
+
+      const listbox = screen.getByRole('listbox');
+      expect(listbox).toBeInTheDocument();
+
+      const options = within(listbox).getAllByRole('option');
+
+      expect(options).toHaveLength(2);
+
+      await user.click(options[1]);
+      expect(select).toHaveTextContent('Completion');
+
+      const checkbox = await screen.findByRole('checkbox');
+      await user.click(checkbox);
+
+      const submitBtn = await screen.findByRole('button', {
+         name: /edit subject/i,
+      });
+      await user.click(submitBtn);
+   });
+   it('subject should be deleted', async () => {
+      const user = userEvent.setup();
+      render(<SubjectsPage />);
+      const subjects = await screen.findAllByLabelText('subject');
+      expect(subjects).toHaveLength(3);
+      const subject = subjects[0];
+
+      const optionsBtn = await within(subject).findByLabelText(/open options/i);
+      await user.click(optionsBtn);
+
+      const deleteBtn = await screen.findByRole('button', { name: /delete/i });
+      expect(deleteBtn).toBeInTheDocument();
+      await userEvent.click(deleteBtn);
+
+      expect(
+         await screen.findByText('Subject deleted successsfully'),
+      ).toBeInTheDocument();
+   });
+});
--- src/test/server/serverHandlers.ts
+++ src/test/server/serverHandlers.ts
@@ -1,5 +1,6 @@
 import { HttpResponse, PathParams, http } from 'msw';
 import { User } from '../../types/UserModel';
+import { AddSubjectFields, EditSubjectFields } from '../../types/schemas';
 
 type LoginRequestBody = {
    email: string;
@@ -146,4 +147,105 @@ export const handlers = [
          { status: 200 },
       );
    }),
+   http.get('http://localhost:3000/subject', async () => {
+      return HttpResponse.json(
+         [
+            {
+               _id: '663cf649d64201f94f27a922',
+               tasks: [
+                  {
+                     _id: '663cf652d64201f94f27a928',
+                     title: 'zaliczenie za laboratoria',
+                     subject: '663cf649d64201f94f27a922',
+                     completed: true,
+                     user: '661d5f2a9d21998a6d0ce08d',
+                     __v: 0,
+                     date: '2024-07-06T00:00:00.000Z',
+                  },
+               ],
+               name: 'zaawansowane programowanie obiektowe - wyklady',
+               status: 'noTasks',
+               user: '661d5f2a9d21998a6d0ce08d',
+               type: 'completion',
+               completed: true,
+               __v: 1,
+               grade: null,
+            },
+            {
+               _id: '663cf66dd64201f94f27a935',
+               tasks: [],
+               name: 'zaawansowane programowanie obiektowe - laboratoria',
+               status: 'noTasks',
+               user: '661d5f2a9d21998a6d0ce08d',
+               type: 'grade',
+               grade: 0,
+               __v: 3,
+            },
+            {
+               _id: '663cf677d64201f94f27a93b',
+               tasks: [
+                  {
+                     date: null,
+                     _id: '663cf686d64201f94f27a948',
+                     title: 'zaliczenie za laboratoria',
+                     subject: '663cf677d64201f94f27a93b',
+                     completed: true,
+                     user: '661d5f2a9d21998a6d0ce08d',
+                     __v: 0,
+                  },
+               ],
+               name: 'Api w eksploracji danych - wyklady',
+               status: 'noTasks',
+               user: '661d5f2a9d21998a6d0ce08d',
+               type: 'completion',
+               completed: false,
+               __v: 2,
+               grade: 4,
+            },
+         ],
+         { status: 200 },
+      );
+   }),
+
+   http.post<PathParams, AddSubjectFields, { message: string }>(
+      'http://localhost:3000/subject',
+      async ({ request }) => {
+         const { name } = await request.json();
+         if (name === 'test123') {
+            return HttpResponse.json(
+               { message: 'there was an error' },
+               { status: 500 },
+            );
+         }
+         return HttpResponse.json(
+            { message: 'subject added' },
+            { status: 200 },
+         );
+      },
+   ),
+   http.put<PathParams, EditSubjectFields, { message: string }>(
+      'http://localhost:3000/subject/663cf649d64201f94f27a922',
+      async ({ request }) => {
+         const { name } = await request.json();
+         if (name === 'test123') {
+            return HttpResponse.json(
+               { message: 'there was an error' },
+               { status: 500 },
+            );
+         }
+         return HttpResponse.json(
+            { message: 'subject edited' },
+            { status: 200 },
+         );
+      },
+   ),
+   http.delete(
+      'http://localhost:3000/subject/663cf649d64201f94f27a922',
+      async () => {
+         return HttpResponse.json(
+            { message: 'subject deleted' },
+            { status: 200 },
+         );
+      },
+   ),
 ];
