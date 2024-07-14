65e0c76bf91fd34860ce08368cb02590761a1777
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-07-14 14:52:36 +0200

    add logo to authLayout container

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { BsCheck } from 'react-icons/bs';
 import LoginGithub from '../components/LoginGithub';
 import LoginGoogle from '../components/LoginGoogle';
 
@@ -13,6 +14,12 @@ const AuthLayout = ({ children }: AuthLayout) => {
    return (
       <section className="lg:flex">
          <div className="bg-wh flex min-h-screen flex-col items-center justify-center gap-5 py-5 lg:w-1/3">
+            <div className="flex items-center gap-3 text-green-400">
+               <div className="rounded-full  border-2 border-green-400">
+                  <BsCheck size={30} />
+               </div>
+               <h1 className="text-2xl font-bold">Subject Success</h1>
+            </div>
             {children}
 
             {showLoginOptions && (
