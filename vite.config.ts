552fba282e8f8008510d030e0c7825d8414a14d1
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-07-14 17:44:08 +0200

    add env to vite defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       setupFiles: './src/test/setup.ts',
       globals: true,
    },
+   define: {
+      VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID,
+      VITE_GOOGLE_REDIRECT_URL: process.env.VITE_GOOGLE_REDIRECT_UR,
+      VITE_SERVER_ENDPOINT: process.env.VITE_SERVER_ENDPOINT,
+      VITE_GITHUB_CLIENT_ID: process.env.VITE_GITHUB_CLIENT_ID,
+      VITE_GITHUB_REDIRECT_URL: process.env.VITE_GITHUB_REDIRECT_URL,
+      VITE_BACKEND_SERVER_URL: process.env.VITE_BACKEND_SERVER_URL,
+   },
 });
