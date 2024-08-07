5e1a505658e8b2264a02fecb846a51caac864d0f
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-08-07 15:39:27 +0200

    change responsiveness in subjects page

--- src/pages/Subjects/AddSubject.tsx
+++ src/pages/Subjects/AddSubject.tsx
@@ -5,7 +5,7 @@ const AddSubject = () => {
    return (
       <>
          <ModalComponent
-            buttonClassName="flex min-h-[500px] min-w-[300px] cursor-pointer items-center justify-center rounded-xl border-2 border-dashed border-gray-600 text-xl font-bold text-gray-600 transition hover:border-gray-300"
+            buttonClassName="flex min-h-[500px] min-w-full cursor-pointer items-center justify-center rounded-xl border-2 border-dashed border-gray-600 text-xl font-bold text-gray-600 transition hover:border-gray-300"
             buttonChildren="Add Subject +"
             children={<AddSubjectModalContent />}
             label="add subject"
