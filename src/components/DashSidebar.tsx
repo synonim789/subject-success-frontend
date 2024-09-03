3e5524af34f7226189af4d03e4f1e2efc910f1f3
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-09-03 17:14:21 +0200

    change height of sidebar from 100vh to 100dvh

--- src/components/DashSidebar.tsx
+++ src/components/DashSidebar.tsx
@@ -33,7 +33,7 @@ const DashSidebar = ({
    }, [isSuccess, navigate]);
 
    return (
-      <nav className={cn('min-h-screen', className)}>
+      <nav className={cn('h-[100dvh]', className)}>
          <AnimatePresence>
             {sidebarVisible && (
                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
